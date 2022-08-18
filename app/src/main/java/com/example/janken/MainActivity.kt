f259c31254424295b4064e4c0c05ebd746a31f91
noxtsu <94978861+noxtsu@users.noreply.github.com>
2022-08-19 01:31:57 +0900

    mainToResultIntent

--- app/src/main/java/com/example/janken/MainActivity.kt
+++ app/src/main/java/com/example/janken/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.janken
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.janken.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gu.setOnClickListener { onJankenButtonTapped(it) }
+        binding.choki.setOnClickListener { onJankenButtonTapped(it) }
+        binding.pa.setOnClickListener { onJankenButtonTapped(it) }
+
+    }
+
+    fun onJankenButtonTapped(view: View?) {
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
     }
 }
